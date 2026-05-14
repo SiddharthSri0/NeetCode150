74ba1db743a84097cec81fe875e6170eb7535af3
Siddharth Srivastav <siddharthsrivastav805@gmail.com>
2026-05-14 23:30:39 +0530

    Time Complexity O (1 )

--- Arrays&Hashing/ValidSudoku.cpp
+++ Arrays&Hashing/ValidSudoku.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        for (int row = 0; row < 9; row++) {
-            unordered_set<char> seen;
-            for (int i = 0; i < 9; i++) {
-                if (board[row][i] == '.') continue;
-                if (seen.count(board[row][i])) return false;
-                seen.insert(board[row][i]);
+       for (int i =0 ; i < 9  ; i ++){
+            unordered_set<char> us;
+            for(int j =0 ; j < 9 ; j ++){
+                if(board[i][j] == '.') continue;
+                if(us.count(board[i][j])) return false;
+                
+                us.insert(board[i][j]);
+            }
+       }
+
+        for (int i =0 ; i < 9  ; i ++){
+            unordered_set<char> us;
+            for(int j =0 ; j < 9 ; j ++){
+                if(board[j][i] == '.') continue;
+                if(us.count(board[j][i])) return false;
+               
+                us.insert(board[j][i]);
+            }
+       }
+
+       for(int block=0 ; block < 9 ;  block ++){
+        unordered_set<char> us;
+        for(int i =0 ; i < 3 ; i ++){
+            for(int j=0 ; j< 3 ; j++){
+                int row = (block/3) * 3 +i;
+                int column = (block % 3 ) * 3 +j;
+                if(board[row][column] == '.') continue;
+                if(us.count(board[row][column])) return false;
+                us.insert(board[row][column]);
+
             }
         }
-
-        for (int col = 0; col < 9; col++) {
-            unordered_set<char> seen;
-            for (int i = 0; i < 9; i++) {
-                if (board[i][col] == '.') continue;
-                if (seen.count(board[i][col])) return false;
-                seen.insert(board[i][col]);
-            }
-        }
-
-        for (int square = 0; square < 9; square++) {
-            unordered_set<char> seen;
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    int row = (square / 3) * 3 + i;
-                    int col = (square % 3) * 3 + j;
-                    if (board[row][col] == '.') continue;
-                    if (seen.count(board[row][col])) return false;
-                    seen.insert(board[row][col]);
-                }
-            }
-        }
-
-        return true;
+       }
+       return true; 
     }
 };
